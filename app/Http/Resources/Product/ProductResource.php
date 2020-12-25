bf7816fd0cb7902ae253881a90d95c0146c57210
skipzenni <skipzenni@gmail.com>
2020-12-25 10:34:51 +0700

    Product Resource Created

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->detail,
+            'price' => $this->price,
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+            'discount' => $this->discount,
+            'rating' =>  $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            'totalPrice' => round(( 1 - ($this->discount/100)) * $this->price,2),
+            'href' => [
+                'reviews' =>route('reviews.index',$this->id)
+            ]
+        ];
+    }
+}
